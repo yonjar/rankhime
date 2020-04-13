67df9da7c867bbb632f6344a94bc8d16c445fdc5
yonjar <1332934648@qq.com>
2020-04-14 00:26:44 +0800

    更新noctchill四人

--- src/components/G2radius.vue
+++ src/components/G2radius.vue
@@ -1,28 +1,18 @@
 <template>
-<div>
-  <v-card>     
-    <p id="mountNode"/>
-    <v-card-actions>
+  <div>
+    <v-card>
+      <p id="mountNode" />
+      <v-card-actions>
         <v-btn @click="$emit('goback')">返回</v-btn>
-        <v-btn
-            color="green lighten-2"
-            dark
-            @click="dialog=true"
-          >
-            编辑名字
-          </v-btn>
-    </v-card-actions>    
-  </v-card>
-  <v-dialog
-      v-model="dialog"
-      width="500"
-    >
+        <v-btn color="green lighten-2" dark @click="dialog = true">
+          编辑名字
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-dialog v-model="dialog" width="500">
       <v-card>
         <v-card-text>
-            <v-text-field
-                label="输入名字"
-                v-model="pname"
-            ></v-text-field>
+          <v-text-field label="输入名字" v-model="pname"></v-text-field>
         </v-card-text>
         <v-divider></v-divider>
         <v-card-actions>
@@ -30,14 +20,17 @@
           <v-btn
             color="primary"
             flat
-            @click="dialog = false;drawChart()"
+            @click="
+              dialog = false;
+              drawChart();
+            "
           >
             ok
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-</div>
+  </div>
 </template>
 
 <script>
@@ -47,89 +40,93 @@ export default {
   data() {
     return {
       chart: null,
-      dialog:false,
-      pname:'你',
+      dialog: false,
+      pname: "你",
       imageColor: {
-        "天海春香": "#e22b30",
-        "如月千早": "#2743d2",
-        "萩原雪歩": "#2743d2",
-        "高槻やよい": "#f39939",
-        "秋月律子": "#01a860",
-        "三浦あずさ": "#9238be",
-        "水瀬伊織": "#fd99e1",
-        "菊地真": "#515558",
+        天海春香: "#e22b30",
+        如月千早: "#2743d2",
+        萩原雪歩: "#2743d2",
+        高槻やよい: "#f39939",
+        秋月律子: "#01a860",
+        三浦あずさ: "#9238be",
+        水瀬伊織: "#fd99e1",
+        菊地真: "#515558",
         "双海亜美/真美": "#ffe43f",
-        "星井美希": "#b4e04b",
-        "我那覇響": "#01adb9",
-        "四条貴音": "#a6126a",
-        "伊吹翼": "#fed552",
-        "エミリー": "#554171",
-        "大神環": "#ee762e",
-        "春日未来": "#ea5b76",
-        "北上麗花": "#6bb6b0",
-        "北沢志保": "#afa690",
-        "木下ひなた": "#d1342c",
-        "高坂海美": "#e9739b",
-        "佐竹美奈子": "#58a6dc",
-        "篠宮可憐": "#b63b40",
-        "島原エレナ": "#9bce92",
-        "ジュリア": "#d7385f",
-        "周防桃子": "#efb864",
-        "高山紗代子": "#7f6575",
-        "田中琴葉": "#92cfbb",
-        "天空橋朋花": "#bee3e3",
-        "徳川まつり": "#5abfb7",
-        "所恵美": "#454341",
-        "豊川風花": "#7278a8",
-        "中谷育": "#f7e78e",
-        "永吉昴": "#aeb49c",
-        "七尾百合子": "#c7b83c",
-        "二階堂千鶴": "#f19557",
-        "野々原茜": "#eb613f",
-        "箱崎星梨花": "#ed90ba",
-        "馬場このみ": "#f1becb",
-        "福田のり子": "#eceb70",
-        "舞浜歩": "#e25a9b",
-        "真壁瑞希": "#99b7dc",
-        "松田亜利沙": "#b54461",
-        "宮尾美也": "#b54461",
-        "最上静香": "#6495cf",
-        "望月杏奈": "#7e6ca8",
-        "百瀬莉緒": "#f19591",
-        "矢吹可奈": "#f5ad3b",
-        "横山奈緒": "#788bc5",
-        "ロコ": "#fff03c",
-        "桜守歌織": "#274079",
-        "白石紬": "#ebe1ff",
-        "櫻木真乃": "#ffbad9",
-        "風野灯織": "#1a4887",
-        "八宮めぐる": "#ffe011",
-        "月岡恋鐘": "#f94aad",
-        "田中摩美々": "#a745fb",
-        "白瀬咲耶": "#005d44",
-        "三峰結華": "#3990c4",
-        "幽谷霧子": "#d9f2ff",
-        "小宮果穂": "#e9471c",
-        "園田智代子": "#f9348c",
-        "西城樹里": "#ffc903",
-        "杜野凛世": "#89c4ed",
-        "有栖川夏葉": "#90e667",
-        "大崎甘奈": "#f54074",
-        "大崎甜花": "#e759ec",
-        "桑山千雪": "#fbfbfb",
-        "芹沢あさひ": "#f30100",
-        "黛冬優子": "#5ce626",
-        "和泉愛依": "#ff00ff",
-        "其他": "#ccc"
-    },
+        星井美希: "#b4e04b",
+        我那覇響: "#01adb9",
+        四条貴音: "#a6126a",
+        伊吹翼: "#fed552",
+        エミリー: "#554171",
+        大神環: "#ee762e",
+        春日未来: "#ea5b76",
+        北上麗花: "#6bb6b0",
+        北沢志保: "#afa690",
+        木下ひなた: "#d1342c",
+        高坂海美: "#e9739b",
+        佐竹美奈子: "#58a6dc",
+        篠宮可憐: "#b63b40",
+        島原エレナ: "#9bce92",
+        ジュリア: "#d7385f",
+        周防桃子: "#efb864",
+        高山紗代子: "#7f6575",
+        田中琴葉: "#92cfbb",
+        天空橋朋花: "#bee3e3",
+        徳川まつり: "#5abfb7",
+        所恵美: "#454341",
+        豊川風花: "#7278a8",
+        中谷育: "#f7e78e",
+        永吉昴: "#aeb49c",
+        七尾百合子: "#c7b83c",
+        二階堂千鶴: "#f19557",
+        野々原茜: "#eb613f",
+        箱崎星梨花: "#ed90ba",
+        馬場このみ: "#f1becb",
+        福田のり子: "#eceb70",
+        舞浜歩: "#e25a9b",
+        真壁瑞希: "#99b7dc",
+        松田亜利沙: "#b54461",
+        宮尾美也: "#b54461",
+        最上静香: "#6495cf",
+        望月杏奈: "#7e6ca8",
+        百瀬莉緒: "#f19591",
+        矢吹可奈: "#f5ad3b",
+        横山奈緒: "#788bc5",
+        ロコ: "#fff03c",
+        桜守歌織: "#274079",
+        白石紬: "#ebe1ff",
+        櫻木真乃: "#ffbad9",
+        風野灯織: "#1a4887",
+        八宮めぐる: "#ffe011",
+        月岡恋鐘: "#f94aad",
+        田中摩美々: "#a745fb",
+        白瀬咲耶: "#005d44",
+        三峰結華: "#3990c4",
+        幽谷霧子: "#d9f2ff",
+        小宮果穂: "#e9471c",
+        園田智代子: "#f9348c",
+        西城樹里: "#ffc903",
+        杜野凛世: "#89c4ed",
+        有栖川夏葉: "#90e667",
+        大崎甘奈: "#f54074",
+        大崎甜花: "#e759ec",
+        桑山千雪: "#fbfbfb",
+        芹沢あさひ: "#f30100",
+        黛冬優子: "#5ce626",
+        和泉愛依: "#ff00ff",
+        市川雛菜: "#f9cf49",
+        浅倉透: "#65c4c6",
+        樋口円香: "#c8164a",
+        福丸小糸: "#7265ab",
+        其他: "#ccc"
+      }
     };
   },
   props: ["idols"],
-  
+
   mounted() {
     setTimeout(() => {
       this.drawChart();
-    this.loading=false
+      this.loading = false;
     }, 10);
   },
   methods: {
@@ -150,7 +147,7 @@ export default {
         container: "mountNode",
         forceFit: true,
         padding: "auto",
-        height:"300"
+        height: "600"
       });
 
       this.chart.source(this.dv);
@@ -198,7 +195,7 @@ export default {
         alignX: "middle",
         alignY: "middle"
       });
-      this.chart.legend(false)
+      this.chart.legend(true);
       this.chart.render();
     }
   }
